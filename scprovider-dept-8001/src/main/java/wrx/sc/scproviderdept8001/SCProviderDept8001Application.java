124d4e83bd8353e964619dbb06d83d0042777083
wuruixiong <wowuruixiong@163.com>
2020-07-24 23:43:39 +0800

    add eureka service

--- scprovider-dept-8001/src/main/java/wrx/sc/scproviderdept8001/SCProviderDept8001Application.java
+++ scprovider-dept-8001/src/main/java/wrx/sc/scproviderdept8001/SCProviderDept8001Application.java
@@ -2,11 +2,14 @@ package wrx.sc.scproviderdept8001;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-/*@EnableEurekaClient //在服务启动后自动注册到Eureka中！
-@EnableDiscoveryClient //服务发现~
-@EnableCircuitBreaker //*/
+@EnableEurekaClient //在服务启动后自动注册到Eureka中！
+//@EnableDiscoveryClient //服务发现~
+//@EnableCircuitBreaker //
 public class SCProviderDept8001Application {
 
     public static void main(String[] args) {
