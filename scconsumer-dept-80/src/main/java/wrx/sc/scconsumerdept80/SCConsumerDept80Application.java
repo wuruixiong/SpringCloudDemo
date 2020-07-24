133ff8f3778cc7a4f8042607deff784109c03d65
wuruixiong <wowuruixiong@163.com>
2020-07-24 09:45:35 +0800

    add consumer

---
+++ scconsumer-dept-80/src/main/java/wrx/sc/scconsumerdept80/SCConsumerDept80Application.java
@@ -0,0 +1,13 @@
+package wrx.sc.scconsumerdept80;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SCConsumerDept80Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SCConsumerDept80Application.class, args);
+    }
+
+}
--- scprovider-dept-8001/src/main/java/wrx/sc/scproviderdept8001/SCProviderDept8001Application.java
+++ scprovider-dept-8001/src/main/java/wrx/sc/scproviderdept8001/SCProviderDept8001Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*@EnableEurekaClient //在服务启动后自动注册到Eureka中！
+@EnableDiscoveryClient //服务发现~
+@EnableCircuitBreaker //*/
 public class SCProviderDept8001Application {
 
     public static void main(String[] args) {
