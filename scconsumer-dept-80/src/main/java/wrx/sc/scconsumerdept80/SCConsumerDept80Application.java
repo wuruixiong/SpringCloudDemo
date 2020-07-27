f52565e3b6e0787bc6eca13c38f38430e04d95c9
wuruixiong <wowuruixiong@163.com>
2020-07-27 19:22:46 +0800

    add ribbon

--- scconsumer-dept-80/src/main/java/wrx/sc/scconsumerdept80/SCConsumerDept80Application.java
+++ scconsumer-dept-80/src/main/java/wrx/sc/scconsumerdept80/SCConsumerDept80Application.java
@@ -2,8 +2,13 @@ package wrx.sc.scconsumerdept80;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+//在微服务启动的时候就能去加载我们自定义Ribbon类
+//@RibbonClient
 public class SCConsumerDept80Application {
 
     public static void main(String[] args) {
--- scconsumer-dept-80/src/main/java/wrx/sc/scconsumerdept80/controller/DeptConsumerController.java
+++ scconsumer-dept-80/src/main/java/wrx/sc/scconsumerdept80/controller/DeptConsumerController.java
@@ -20,8 +20,8 @@ public class DeptConsumerController {
     private RestTemplate restTemplate; //提供多种便捷访问远程http服务的方法，简单的Restful服务模板~
 
     //Ribbon。我们这里的地址，应该是一个变量，通过服务名来访问
-    private static final String REST_URL_PREFIX = "http://localhost:8001";
-    //private static final String REST_URL_PREFIX = "http://SPRINGCLOUD-PROVIDER-DEPT";
+    //private static final String REST_URL_PREFIX = "http://localhost:8001";
+    private static final String REST_URL_PREFIX = "http://SPRINGCLOUD-PROVIDER-DEPT";
 
     @RequestMapping("/consumer/dept/add")
     public boolean add(Dept dept){
