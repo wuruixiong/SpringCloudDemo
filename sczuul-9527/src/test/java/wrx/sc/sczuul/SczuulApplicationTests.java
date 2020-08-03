8fb40118adc589cffad710ee6d45e95c3d1b56d8
wuruixiong <wowuruixiong@163.com>
2020-08-03 13:36:24 +0800

    add zuul

---
+++ sczuul-9527/src/test/java/wrx/sc/sczuul/SczuulApplicationTests.java
@@ -0,0 +1,13 @@
+package wrx.sc.sczuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SczuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
